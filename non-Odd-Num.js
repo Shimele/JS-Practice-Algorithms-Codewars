a939ecb4d7f2fc8bd0fd90c0b83721cdb9fc7ab4
shimele <bechembayamba@gmail.com>
2020-02-11 11:22:31 +0100

    find all non odd numbers in array

---
+++ non-Odd-Num.js
@@ -0,0 +1,18 @@
+/*
+Write a small function that returns the values of an array that are not odd.
+
+All values in the array will be integers. Return the good values in the order they are given.
+
+function noOdds( values )
+*/
+function noOdds( values ){
+    let nonOddNum = []
+        for(let i = 0; i < values.length; i++){
+            if(values[i] % 2 == 0){
+                nonOddNum.push(values[i])
+            }
+        }
+    return nonOddNum
+}
+//alternative
+var noOdds = values => values.filter(x => x % 2 === 0);
