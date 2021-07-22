db3bc8d6e1ed3c6a1b33922db2489aeb8dfbbda4
shimele <bechembayamba@gmail.com>
2021-07-22 07:58:26 +0100

    new kata solved, cost of renting my car

--- validSpaceString.js
+++ validSpaceString.js
@@ -1,6 +1,10 @@
-//Your task is to write a function called valid_spacing() or validSpacing() which checks if a string has valid spacing. The function should return either True or False.
+//Your task is to write a function called valid_spacing()
+// or validSpacing() which checks if a string has valid spacing.
+//The function should return either True or False.
 
-//For this kata, the definition of valid spacing is one space between words, and no leading or trailing spaces. Below are some examples of what the function should return.
+//For this kata, the definition of valid spacing is one
+// space between words, and no leading or trailing spaces.
+// Below are some examples of what the function should return.
 
 function validSpacing(s) {
   let before = s.length;
